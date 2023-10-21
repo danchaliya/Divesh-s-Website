5a8d043a00b838a7c4d356af7fa2bc37cc9c81cf
Divesh Anchaliya <125212193+danchaliya@users.noreply.github.com>
2023-10-21 11:39:04 -0400

    Create Menu.js

---
+++ components/Menu.js
@@ -0,0 +1,10 @@
+export default function Menu() {
+    return (
+        <nav className="menu">
+            <a href="#welcome">Welcome</a>
+            <a href="#whoiam">Who I am</a>
+            <a href="#whatido">What I do</a>
+            <a href="#getintouch">Get in Touch</a>
+        </nav>
+    );
+}
