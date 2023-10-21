fff7d53b80c38a64be8e64fa2b2553ad89507e33
Divesh Anchaliya <125212193+danchaliya@users.noreply.github.com>
2023-10-21 11:44:44 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Menu from '@components/Menu';
+
 
 export default function Home() {
   return (
