f3542ed9cc7a093b8ce3083b242c41e6478334c6
Divesh Anchaliya <125212193+danchaliya@users.noreply.github.com>
2023-11-10 16:56:46 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -53,20 +53,40 @@ export default function Home() {
           <h2>Recent Projects</h2>
           <div className="project-card">
             <h3>Project 1: Elon Open Data Service(ODS)</h3>
+            <h4>Software Engineer</h4>
             <p>Built an API for students at Elon which extracts Data from Elon Servers regarding class schedules, building times, dining hall menus and allows student developers to build an application using the API to gather data from Elon's Servers
 Created a design document that outlined the steps needed to build the API and included specific endpoints for the API to retrieve information
 Worked on writing code that would authenticate users through the use of SAML Authentication and the use of Cookies.</p>
           </div>
           <div className="project-card">
             <h3>Project 2:KiddyKam </h3>
-            <p>Description of the second project, highlighting its features and technologies used.</p>
+            <h4>Software Engineer</h4>
+            <p></p>
           </div>
+        <div className="project-card">
+            <h3>Project 3:HerdsWallet  </h3>
+            <h4> Creator and Software Engineer</h4>
+            <p>Created the idea of a financial tracking app and worked with a team of 4 to create an application in 36 hours for the 2023 Hackabull Hackaton at the University of South Florida.  
+Created the front-end in Java Script and TypeScript
+Helped with routing the backend to the frontend
+Helped incoporate the use of MongoDB into the backend</p>
+          </div>
+  <div className="project-card">
+            <h3>Project 4:ZenZone </h3>
+            <h4> Creator and Software Engineer</h4>
+            <p>Created the idea of a guided meditation app and worked with a team of 4 to create an application in 48 hours
+Created the backend in Java. 
+Helped with routing the backend to the frontend
+Helped incoporate the use of MongoDB into the backend</p>
+          </div>
+  
         </section>
 
         <section id="contact" className="contact">
           <h2>Contact Me</h2>
           <p>Feel free to reach out if you have questions or just want to chat! Email me at: <a href="mailto:divesh.nc@gmail.com">divesh.nc@gmail.com</a></p>
           <p>Phone: 919-397-8162</p>
+          <p> LinkedIn: <a href=https://www.linkedin.com/in/mira-martinez-1828591ab/ </p>
         </section>
       </main>
 
